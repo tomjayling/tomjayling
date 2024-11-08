94ab922994c2dc7beda7829dd9346230186e1136
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:02:12 +0000

    Add files via upload

---
+++ Learning C#/ProgramFlow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "";
+
+            // System.Console.WriteLine("Basic while() loop:");
+            // while (inputStr != "exit") {
+            //     inputStr = Console.ReadLine();
+            //     System.Console.WriteLine("You entered: {0}", inputStr);
+            // }
+
+            System.Console.WriteLine("The do-while() loop:");
+            do {
+                inputStr = Console.ReadLine();
+                System.Console.WriteLine("You entered: {0}", inputStr);
+            } while (inputStr != "exit");
+        }
+    }
+}
