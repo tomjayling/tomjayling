6fc19ff1e7d943c06d8e3180c3736e12f160bb00
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:05:43 +0000

    Add files via upload

---
+++ Learning C#/Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outstr;
+            string str1 = "the quick brown fox jumps over the lazy dog.";
+            string str2 = "This is a string";
+            string str3 = "THIS is a STRING";
+            string[] strs = {"one", "two", "three", "four"};
+
+            System.Console.WriteLine(str1.Length);
+
+            System.Console.WriteLine(str1[14]);
+
+            foreach (char c in str1){
+                System.Console.Write(c);
+                if (c == 'b'){
+                    System.Console.WriteLine();
+                    break;
+                }
+            }
+
+            outstr = String.Concat(strs);
+            System.Console.WriteLine(outstr);
+
+            outstr = String.Join('.', strs);
+            System.Console.WriteLine(outstr);
+
+            outstr = String.Join("---", strs);
+            System.Console.WriteLine(outstr);
+
+
+            int result = String.Compare(str2, "This is a string");
+            System.Console.WriteLine(result);
+
+            bool isEqual = str2.Equals(str3);
+            System.Console.WriteLine(isEqual);
+
+            System.Console.WriteLine(str1.IndexOf('e'));
+            System.Console.WriteLine(str1.IndexOf("fox"));
+            System.Console.WriteLine(str1.LastIndexOf('e'));
+            System.Console.WriteLine(str1.LastIndexOf("the"));
+
+            outstr = str1.Replace("fox", "cat");
+            System.Console.WriteLine(outstr);
+            System.Console.WriteLine(outstr.IndexOf("fox"));
+        }
+    }
+}
