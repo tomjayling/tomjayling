55877911a11344121f57a633ff355dcefda0d5a7
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:08:31 +0000

    Add files via upload

---
+++ Learning C#/Functions/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+void PrintWithPrefix(string thestr, string prefix = "") {
+    System.Console.WriteLine($"{prefix} {thestr}");
+}
+
+PrintWithPrefix("Hello There!");
+PrintWithPrefix("Hello There!", ">:");
+
+PrintWithPrefix(prefix: "% ", thestr: "Hello There!");
+
