7518e22dd3eeb5f166151c3190687986b81f8f01
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 14:57:56 +0000

    Add files via upload

---
+++ Learning C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = {15, 7, 12, 23, 41, 28, 9, 17, 36};
+
+            System.Console.WriteLine("Using break and continue:");
+            foreach (int val in values)
+            {
+                if (val >= 20 && val <= 29){
+                    continue; //skips the rest of the iteration
+                }
+
+
+                System.Console.WriteLine($"val is curently {val}");
+                if (val >= 40){
+                    break; //skips all further iterations
+                }
+            }
+
+
+        }
+    }
+}
