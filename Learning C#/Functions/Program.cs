6cdf6d0c1a5ce20ada5c3f46caf9619042d5b8c7
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:09:52 +0000

    Add files via upload

---
+++ Learning C#/Functions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RefAndOutParams {
+
+    class Program
+    {
+        static void TestFunc1(int arg1){
+            arg1 += 10;
+            System.Console.WriteLine($"{arg1}");
+        }
+
+        static void TestFunc2(ref int arg1){
+            arg1 += 10;
+            System.Console.WriteLine($"{arg1}");
+        }
+
+        static void PlusTimes(int arg1, int arg2, out int sum, out int product) {
+            sum = arg1 + arg2;
+            product = arg1 * arg2;
+
+        }
+
+
+        static void Main(string[] args){
+            int val1 = 10;
+            int val2 = 20;
+
+            TestFunc1(val1);
+            System.Console.WriteLine(val1);
+
+            TestFunc2(ref val1);
+            System.Console.WriteLine(val1);
+
+            int a, b;
+            PlusTimes(val1, val2, out a, out b);
+            System.Console.WriteLine($"{a}, {b}");
+        }
+    }
+}
