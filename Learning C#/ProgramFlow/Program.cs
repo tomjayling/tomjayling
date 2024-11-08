8d82fa4ee0049b6a7a65fbe82fd2cc3e4f37a68d
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:00:52 +0000

    Add files via upload

---
+++ Learning C#/ProgramFlow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVal = 15;
+            int[] nums = new int[] {3, 14, 15, 92, 6};
+            string str = "the quick brown fox jumps over the lazy dog";
+
+            // System.Console.WriteLine("The basic for loop");
+            // for (int i = 0; i < myVal; i++)
+            // {
+            //     System.Console.WriteLine("i is currently {0}", i);
+            // }
+
+            // foreach (int i in nums){
+            //     System.Console.WriteLine("i is currently {0}", i);
+            // }
+
+            var count = 0;
+            foreach (char c in str){
+                if (c=='o') {
+                    count++;
+                }
+            }
+            System.Console.WriteLine("Counted {0} o characters", count);
+        }
+    }
+}
