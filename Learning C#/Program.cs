999bf2488d9a07c5386a246bc30a060fb0cc4260
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:07:05 +0000

    Add files via upload

---
+++ Learning C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+float MilesToKm(float miles){
+    float result = miles * 1.6f;
+    return result;
+}
+
+void PrintWithPrefix(string theStr) {
+    System.Console.WriteLine($"::> {theStr}");
+}
+
+
+System.Console.WriteLine($"The result is {MilesToKm(8.0f)}");
+System.Console.WriteLine($"The result is {MilesToKm(52.0f)}");
+
+PrintWithPrefix("Test String");
+PrintWithPrefix("Another Test String");
