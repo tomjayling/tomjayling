e44ee3be9dbc9b27ede0a5848045f63414a2fef8
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:16:48 +0000

    Add files via upload

---
+++ Learning C#/Classes/StringRep/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringRep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000;
+            System.Console.WriteLine(x.ToString());
+
+            object a = new object();
+            System.Console.WriteLine(a.ToString());
+
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            System.Console.WriteLine(b1.ToString());
+            System.Console.WriteLine(b1);
+
+            System.Console.WriteLine(b1.ToString('B'));
+            System.Console.WriteLine(b1.ToString('F'));
+        }
+    }
+}
