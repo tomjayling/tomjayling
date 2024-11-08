45c01b565d6c64e2ea8f165f14902b2d375dd7a8
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:14:41 +0000

    Add files via upload

---
+++ Learning C#/Classes/Modifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+
+            // b1._name = "Aldous Huxley";
+
+            // System.Console.WriteLine(b1.GetDescription());
+
+            b1.SetName("Grapes of Wrath");
+            b1.SetAuthor("John Steinbeck");
+            b1.SetPages(464);
+            System.Console.WriteLine(b1.GetDescription());
+           
+        }
+    }
+}
