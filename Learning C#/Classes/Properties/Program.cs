0ada711f3d90917a5c627644e820a70dda2503b1
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:15:43 +0000

    Add files via upload

---
+++ Learning C#/Classes/Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+
+            System.Console.WriteLine(b1.Name);
+            System.Console.WriteLine(b1.Description);
+
+            b1.ISBN = "100140447938";
+            b1.Price = 24.95m;
+            System.Console.WriteLine(b1.ISBN);
+            System.Console.WriteLine(b1.Price);
+
+            b1.Name = "Crime and Punishment";
+            b1.Pagecount = 652;
+            System.Console.WriteLine(b1.Description);
+            System.Console.WriteLine(b1.Name);
+            System.Console.WriteLine(b1.Pagecount);
+        }
+    }
+}
