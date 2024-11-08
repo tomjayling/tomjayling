2dab29da6b0a8710b062bfa5f7c73f80674d7d8f
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:00:19 +0000

    Add files via upload

---
+++ Learning C#/ProgramFlow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int x = 10000;
+            int y = 0;
+            int result;
+
+            try {
+                if (x > 1000){
+                    throw new ArgumentOutOfRangeException("x","x has to be 1000 or less");
+                }
+                result = x/y;
+                System.Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e){
+                System.Console.WriteLine("Whoops!");
+                System.Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e) {
+                System.Console.WriteLine("Sorry, 1000 is the limit");
+                System.Console.WriteLine(e.Message);
+            }
+            finally {
+                System.Console.WriteLine("This code always runs");
+            }
+        }
+    }
+}
