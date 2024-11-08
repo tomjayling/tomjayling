b77c79fdf7529ef5b4151fc6e790e70a41b5b2b9
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 14:59:29 +0000

    Add files via upload

---
+++ Learning C#/ProgramFlow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 51;
+
+            // if (theVal == 50){
+            //     System.Console.WriteLine("theVal is 50");
+            // }
+            // else if (theVal >= 51 && theVal <= 60){
+            //     System.Console.WriteLine("theVal is between 51 and 60");
+            // }
+            // else {
+            //     System.Console.WriteLine("theVal is something else");
+            // }
+
+            System.Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
+        }
+    }
+}
