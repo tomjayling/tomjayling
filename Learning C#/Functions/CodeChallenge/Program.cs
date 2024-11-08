00937ddb3549825b0c877e162a00bc389cdf61b4
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:08:07 +0000

    Add files via upload

---
+++ Learning C#/Functions/CodeChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is how your code will be called.
+// You can edit this code to try different testing cases.
+string[] teststrings = { "Hello World!", "Race car!", "Rotor", "More cowbell!", "Madam, I'm Adam." };
+int palcount = 0;
+foreach (string str in teststrings) {
+	bool learnerResult = Answer.IsPalindrome(str);
+	if (learnerResult)
+		palcount++;
+}
+if (palcount == 3){
+    System.Console.WriteLine("Correct");
+}
+else {
+    System.Console.WriteLine("Incorrect");
+}
