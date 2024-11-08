ddbea4dbac1eaf427b5dff5e79cbe933e6189f6a
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:09:07 +0000

    Add files via upload

---
+++ Learning C#/Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultiValues{
+
+    class Program 
+    {
+        static void Main(string[] args){
+
+            (int a, int b) tup1 = (5,10);
+            var tup2 = ("Some text", 10.5f);
+
+            tup1.b = 20;
+            tup2.Item1 = "New String";
+
+            System.Console.WriteLine($"{tup1.a}, {tup1.b}");
+            System.Console.WriteLine($"{tup2.Item1}, {tup2.Item2}");
+
+            (int, int) result = PlusTimes(6,12);
+            System.Console.WriteLine($"Sum: {result.Item1}, Product: {result.Item2}");
+
+        }
+
+        static (int, int) PlusTimes(int a, int b) {
+            return (a+b, a*b);
+        }
+    }
+}
