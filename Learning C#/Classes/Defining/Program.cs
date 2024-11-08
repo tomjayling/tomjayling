311877acd955245b48cfc2ccca56ce431864a358
Thomas Ayling <ayling.thomasj@gmail.com>
2024-11-08 15:12:22 +0000

    Add files via upload

---
+++ Learning C#/Classes/Defining/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Defining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Book b2 = new Book("The Grapes of Wrath", "John Steinbeck", 465);
+
+            System.Console.WriteLine(b1.GetDescription());
+            System.Console.WriteLine(b2.GetDescription());
+
+            //b1._name = "Aldous Huxley"; ---This line does not work
+        }
+    }
+}
